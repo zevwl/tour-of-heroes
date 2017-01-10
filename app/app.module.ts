70e5446ea2ba92eb8134205eb543d0768d5bda72
zevwl <zevwl@bhphoto.com>
2017-01-09 22:22:28 -0500

    Step 6: HTTP

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,18 @@
+import './rxjs-extensions';
+
 import { NgModule }      from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './shared/in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './shared/hero.service';
 
@@ -14,13 +22,16 @@ import { AppRoutingModule } from './app-routing.module';
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
     declarations: [
         AppComponent,
         DashboardComponent,
         HeroesComponent,
-        HeroDetailComponent
+        HeroDetailComponent,
+        HeroSearchComponent
     ],
     providers: [
         HeroService
